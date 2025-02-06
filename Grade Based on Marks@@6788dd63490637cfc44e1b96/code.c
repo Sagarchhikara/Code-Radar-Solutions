bd2d68c9ae74d21eb96f742f008287ca3eb10ad0
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-02-06 12:02:16 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -4,19 +4,22 @@ int main() {
     int marks;
     scanf("%d",&marks);
     if(marks>=90){
-        printf('A');
+        printf("A\n");
     }
     else if( marks>=80 && marks<90){
-        printf('B');
+        printf("B\n");
     }
     else if(marks>=70 && marks<80){
-        printf('C');
+        printf("C\n");
     }
     else if(marks>=60 && marks<70){
-        printf('D');
+        printf("D\n");
     }
-    else{
-        printf('F');
+    else if (marks<60){
+        printf("F\n");
+    }
+    else {
+        printf("Invalid marks entered\n");
     }
     
     return 0;
