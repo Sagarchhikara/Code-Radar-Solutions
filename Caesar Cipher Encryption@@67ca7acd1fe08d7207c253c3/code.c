418e209fe50cb714e76c3267ac33a551ad2f169c
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-03-09 04:44:32 +0530

    Solution to Caesar Cipher Encryption

---
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -0,0 +1,11 @@
+void caesarCipher(char message[],int key){
+    int i;
+    for(i=0;i<strlen(message);i++){
+        if(message[i]>='A'&& message[i]<='Z'){
+            message[i]=((message[i]-'A'+key)%26)+'A'
+        }
+        else if(message[i]>='a' && message[i]<='z'){
+            message[i]=((message[i]-'a'+key)%26)+'a'
+        }
+    }
+}
