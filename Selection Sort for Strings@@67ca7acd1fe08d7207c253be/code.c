cd28ad41d6686ab5002e5fcd5dac25ef00381da3
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-03-12 10:14:36 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -1,26 +1,28 @@
 #include<stdio.h>
 #include<string.h>
-#define MAX_STRINGS 100
-#define MAX_LENGTH 100
-void selectionSort(char arr[][MAX_LENGTH],int n){
-    int i,j,min_idx;
-    for(i=0;i<n-1;i++){
-        min_idx=i;
+#define MAX 100
+#define SIZE 100
+void swap(char str1[],char str2[]){
+    char temp[MAX];
+    strcpy(temp,str1);
+    strcpy(str1,str2);
+    strcpy(str2,temp);
+}
+void selectionSort(char arr[][MAX],int n){
+    int minIndex;
+    for( int i=0;i<n-1;i++){
+        minIndex=i;
         for(j=i+1;j<n;j++){
-            if(strcmp(arr[j],arr[min_idx])<0){
-                min_idx=j;
+            if(strcmp(arr[j],arr[minIndex])<0){
+                minIndex=j;
             }
         }
         if(min_idx!=i){
-            char temp[MAX_LENGTH];
-            strcpy(temp,arr[i]);
-            strcpy(arr[i],arr[min_idx]);
-            strcpy(arr[min_idx],temp);
-        }
+            swap(arr[i],arr[minIndex])
     }
 }
-void printArray(char arr[][MAX_LENGTH],int n){
+void printArray(char arr[][MAX],int n){
     for(int i=0;i<n;i++){
-        printArray("%c\n",arr[i]);
+        printArray("%s\n",arr[i]);
     }
 }
