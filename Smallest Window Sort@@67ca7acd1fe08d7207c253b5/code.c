f254672e9fb378c314375a1909bc0db09eaac7aa
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-03-12 09:57:15 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,35 +1,50 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<limits.h>
-void findUnsortedSubarray(int arr[],int a){
-    if(a<=1){
-        printf("0\n");
-        return;
-    }
-    int left =0;
-    while(left < a-1 && arr[left] <=arr[left+1]){
-        left++;
-    }
-    if(left ==a-1){
-        printf("0\n");
-        return;
-    }
-    int right= a-1;
-    while(right>0&&arr[right]>= arr[right-1]){
-        right--;
-    }
-    int min_val=INT_MAX;
-    int max_val=INT_MIN;
-    for(int i=left;i<=right;i++){
-        if(arr[i]<min_val){
-            min_val=arr[i];
+int findUnsortedSubarray(int arr[], int n) {
+    int start = -1, end = -1;
+
+    // Find the first element which is greater than the next element (start of unsorted part)
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            start = i;
+            break;
         }
     }
-    while(left>0&& arr[left-1]>min_val){
-        left--;
+
+    // If the array is already sorted
+    if (start == -1) {
+        return 0;
     }
-    while(right<a-1&& arr[right+1]<max_val){
-        right++;
+
+    // Find the last element which is smaller than the previous element (end of unsorted part)
+    for (int i = n - 1; i > 0; i--) {
+        if (arr[i] < arr[i - 1]) {
+            end = i;
+            break;
+        }
     }
-    printf("%d\n",right-left+1);
+
+    // Find the min and max within the unsorted subarray
+    int minVal = arr[start], maxVal = arr[start];
+    for (int i = start; i <= end; i++) {
+        if (arr[i] < minVal) minVal = arr[i];
+        if (arr[i] > maxVal) maxVal = arr[i];
+    }
+
+    // Expand the left boundary if needed
+    for (int i = 0; i < start; i++) {
+        if (arr[i] > minVal) {
+            start = i;
+            break;
+        }
+    }
+
+    // Expand the right boundary if needed
+    for (int i = n - 1; i > end; i--) {
+        if (arr[i] < maxVal) {
+            end = i;
+            break;
+        }
+    }
+
+    // Length of the subarray that needs to be sorted
+    return (end - start + 1);
 }
