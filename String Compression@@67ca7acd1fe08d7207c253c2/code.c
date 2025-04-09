533a0b4636c61abd11c0235f1d65e7a9d4738cc7
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-04-09 21:03:36 +0530

    Solution to String Compression

---
+++ String Compression@@67ca7acd1fe08d7207c253c2/code.c
@@ -0,0 +1,39 @@
+// Your code here...
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void compressString(char* str, char* compressed) {
+    int len = strlen(str);
+    if (len == 0) {
+        compressed[0] = '\0';
+        return;
+    }
+
+    int count = 1;
+    int pos = 0;
+    
+    for (int i = 1; i <= len; i++) {
+        if (i < len && str[i] == str[i-1]) {
+            count++;
+        } else {
+            // Add character to compressed string
+            compressed[pos++] = str[i-1];
+            
+            // Add count if greater than 1
+            if (count > 1) {
+                // Convert count to string and add to compressed
+                char countStr[20];
+                sprintf(countStr, "%d", count);
+                int countLen = strlen(countStr);
+                strncpy(compressed + pos, countStr, countLen);
+                pos += countLen;
+            }
+            
+            count = 1; // Reset count for next character
+        }
+    }
+    
+   
+    return 0;
+}
