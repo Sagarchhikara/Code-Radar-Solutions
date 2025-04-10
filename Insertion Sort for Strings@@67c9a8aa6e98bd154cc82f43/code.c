2852a4f4cbb893b4e8a5329ab634ab4805396d7a
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-04-10 15:17:06 +0530

    Solution to Insertion Sort for Strings

--- Insertion Sort for Strings@@67c9a8aa6e98bd154cc82f43/code.c
+++ Insertion Sort for Strings@@67c9a8aa6e98bd154cc82f43/code.c
@@ -1,13 +1,21 @@
-void insertionSort(char arr[][100], int n) {
-    for (int i = 1; i < n; i++) {
-        char key[100];
-        strcpy(key, arr[i]);
-        int j = i - 1;
-                while (j >= 0 && strcmp(arr[j], key) > 0) {
-            strcpy(arr[j + 1], arr[j]);
+#include <stdio.h>
+#include <string.h>
+
+// Function to perform insertion sort on an array of strings in lexicographical order
+void insertionSort(char *arr[], int n) {
+    int i, j;
+    char *key;
+    
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+        
+        // Move elements that are greater than key to one position ahead of their current position
+        while (j >= 0 && strcmp(arr[j], key) > 0) {
+            arr[j + 1] = arr[j];
             j = j - 1;
         }
-        strcpy(arr[j + 1], key);
+        arr[j + 1] = key;
     }
 }
 void printArray(char arr[][100], int size) {
